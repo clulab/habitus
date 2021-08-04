d2ce5c4e88076e9a4c5ba9592964825c226bc5d5
Mihai Surdeanu <surdeanu@gmail.com>
2021-08-04 12:11:50 +0300

    Added support for custom NERs

--- src/main/scala/org/clulab/variables/VariableProcessor.scala
+++ src/main/scala/org/clulab/variables/VariableProcessor.scala
@@ -4,9 +4,11 @@ import org.clulab.dynet.Utils
 import org.clulab.odin.{ExtractorEngine, Mention}
 import org.clulab.processors.{Document, Processor}
 import org.clulab.processors.clu.CluProcessor
+import org.clulab.sequences.LexiconNER
 
 class VariableProcessor(val processor: Processor, val extractor: ExtractorEngine) {
   def parse(text: String): (Document, Seq[Mention]) = {
+
     // pre-processing
     val doc = processor.annotate(text, keepText = false)
 
@@ -19,9 +21,19 @@ class VariableProcessor(val processor: Processor, val extractor: ExtractorEngine
 
 object VariableProcessor {
   def apply(): VariableProcessor = {
+    // Custom NER for variable reading
+    val kbs = Seq(
+      "variables/FERTILIZER.tsv"
+    )
+    val lexiconNer = LexiconNER(kbs,
+      Seq(
+        true // case insensitive match for fertilizers
+      )
+    )
+
     // create the processor
     Utils.initializeDyNet()
-    val processor: Processor = new CluProcessor()
+    val processor: Processor = new CluProcessor(optionalNER = Some(lexiconNer))
 
     // read rules from yml file in resources
     val source = io.Source.fromURL(getClass.getResource("/variables/master.yml"))
