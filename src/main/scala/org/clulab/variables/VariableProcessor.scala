8b0d93182f520feb49505f4c7d7dfb96e0d99938
mithunpaul08 <mithunpaul08@gmail.com>
2021-09-13 15:13:48 -0700

    will save every context to an arraybuffer and return a seq

--- src/main/scala/org/clulab/variables/VariableProcessor.scala
+++ src/main/scala/org/clulab/variables/VariableProcessor.scala
@@ -6,10 +6,10 @@ import org.clulab.processors.{Document, Processor}
 import org.clulab.processors.clu.CluProcessor
 import org.clulab.sequences.LexiconNER
 
+import scala.collection.mutable.ArrayBuffer
 
-class Context(var location: String, var entity: String, var relativeDist: Int, var count: Int) {
 
-}
+class Context(var location: String, var entity: String, var relativeDist: Int, var count: Int)
 
 class VariableProcessor(val processor: Processor, val extractor: ExtractorEngine) {
   def parse(text: String): (Document, Seq[Mention]) = {
@@ -20,10 +20,12 @@ class VariableProcessor(val processor: Processor, val extractor: ExtractorEngine
     // extract mentions from annotated document
     val mentions = extractor.extractFrom(doc).sortBy(m => (m.sentence, m.getClass.getSimpleName))
     val allContexts = extractContext(doc)
+    println(s"value of allContexts is $allContexts")
     (doc, mentions)
   }
 
-  def extractContext(doc: Document): Unit = {
+  def extractContext(doc: Document): Seq[Context] = {
+   var contexts=new ArrayBuffer[Context](3)
     for ((s, i) <- doc.sentences.zipWithIndex) {
       println(s"sentence #$i")
       println(s.getSentenceText)
@@ -32,13 +34,16 @@ class VariableProcessor(val processor: Processor, val extractor: ExtractorEngine
       println(s"value of blocindexes is $bLocIndexes")
       //val lengths = bLocIndexes.map { start => countLocs(start) }
       println("Tokens: " + (s.words(3)))
-      for ((es,ix) <- s.entities.get.zipWithIndex) {
-          if (es == "B-LOC") {
-            println("Tokens: " + (s.words(ix)))
-          }
-      }
+      for ((es, ix) <- s.entities.get.zipWithIndex) {
+        if (es == "B-LOC") {
+          println("Tokens: " + (s.words(ix)))
+          val ctxt=new Context(s.words(ix),"LOC",ix,1)
+          contexts+=ctxt
+        }
       }
     }
+    (contexts.toSeq)
+  }
 }
 
   object VariableProcessor {
