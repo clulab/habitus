834ce7c7021d9889a8bd8a2e8edba96bb27e539a
mithunpaul08 <mithunpaul08@gmail.com>
2021-09-12 19:22:35 -0700

    found syntax of exact print statements to view entities. added them

--- src/main/scala/org/clulab/utils/package.scala
+++ src/main/scala/org/clulab/utils/package.scala
@@ -36,9 +36,13 @@ package object utils {
   def outputMentionsToTSV(mentions: Seq[Mention], doc: Document, filename: String, pw: PrintWriter): Unit = {
     val mentionsBySentence = mentions groupBy (_.sentence) mapValues (_.sortBy(_.start)) withDefaultValue Nil
     for ((s, i) <- doc.sentences.zipWithIndex) {
-      for (e <-s.entities) {
-        println(s"value of entities is: ${e} with index: ${i}")
-      }
+      println(s"sentence #$i")
+      println(s.getSentenceText)
+      println("Tokens: " + (s.words.indices, s.words, s.tags.get).zipped.mkString(", "))
+      println("Entities: " + s.entities.get.mkString(", "))
+      println("Norms: " + s.norms.get.mkString(", "))
+      printSyntacticDependencies(s)
+      println
       // to keep only mention labelled as Assignment (these labels are associated with .yml files, e.g. Variable, Value)
       val sortedMentions = mentionsBySentence(i).filter(_.label matches "Assignment")
       sortedMentions.foreach{
