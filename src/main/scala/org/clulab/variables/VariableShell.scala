5e9e9032ed6dd1f8e3bc025d87d0c7b2f1b73288
Keith Alcock <github@keithalcock.com>
2021-10-06 09:23:00 -0700

    Arrange for shell to reload...
    ...and show other habitus things.

--- src/main/scala/org/clulab/variables/VariableShell.scala
+++ src/main/scala/org/clulab/variables/VariableShell.scala
@@ -3,26 +3,33 @@ package org.clulab.variables
 import org.clulab.utils._
 
 /** Interactive shell for variable reading */
-class VariableShell() extends Shell {
-
+object VariableShell extends App {
+  println("Creating VariableProcessor...\n")
   private var vp: VariableProcessor = VariableProcessor()
 
-  override def reload(): Unit = {
-    println("reloading VariableProcessor")
+  def reload(menu: Menu, key: String): Boolean = {
+    println("Reloading VariableProcessor...")
     vp = VariableProcessor(vp.processor)
+    true
   }
 
-  override def work(text: String): Unit = {
+  def parse(menu: Menu, text: String): Boolean = {
     // the actual reading
     val (doc, mentions) = vp.parse(text)
 
     // debug display the mentions
     displayMentions(mentions, doc)
+    true
   }
 
-}
+  val lineReader = new CliReader("(Habitus)>>> ", "user.home", ".habitusshellhistory")
+  val mainMenuItems = Seq(
+    new HelpMenuItem(":help", "show commands"),
+    new MainMenuItem(":reload", "reload the variable processor", reload),
+    new ExitMenuItem(":exit", "exit system")
+  )
+  val defaultMenuItem = new DefaultMenuItem(parse)
+  val menu = new Menu("Welcome to the Habitus Shell!", lineReader, mainMenuItems, defaultMenuItem)
 
-object VariableShell extends App {
-  val sh = new VariableShell
-  sh.shell()
+  menu.run()
 }
