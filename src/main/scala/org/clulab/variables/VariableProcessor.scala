fc13f8402c5e8725e43915e4e9ffe5b8e9833bfa
marcovzla <marcos.vzla@gmail.com>
2021-10-05 20:03:18 -0700

    Reload rules in shell (#11)
    * read file if we can find it
    
    * reload

--- src/main/scala/org/clulab/variables/VariableProcessor.scala
+++ src/main/scala/org/clulab/variables/VariableProcessor.scala
@@ -1,5 +1,8 @@
 package org.clulab.variables
 
+import java.io.File
+import java.nio.charset.StandardCharsets
+import org.apache.commons.io.FileUtils
 import org.clulab.dynet.Utils
 import org.clulab.odin.{ExtractorEngine, Mention}
 import org.clulab.processors.{Document, Processor}
@@ -20,6 +23,7 @@ class VariableProcessor(val processor: Processor, val extractor: ExtractorEngine
 }
 
 object VariableProcessor {
+
   def apply(): VariableProcessor = {
     // Custom NER for variable reading
     val kbs = Seq(
@@ -32,19 +36,41 @@ object VariableProcessor {
         true
       )
     )
-
     // create the processor
     Utils.initializeDyNet()
     val processor: Processor = new CluProcessor(optionalNER = Some(lexiconNer))
-
-    // read rules from yml file in resources
-    val source = io.Source.fromURL(getClass.getResource("/variables/master.yml"))
-    val rules = source.mkString
-    source.close()
-
-    // creates an extractor engine using the rules and the default actions
-    val extractor = ExtractorEngine(rules)
-
-    new VariableProcessor(processor, extractor)
+    VariableProcessor(processor)
   }
+
+  /**
+    * Reuse an existing processor
+    *
+    * @param processor
+    * @return
+    */
+  def apply(processor: Processor): VariableProcessor = {
+    // get current working directory
+    val cwd = new File(System.getProperty("user.dir"))
+    // Find resource dir from project root.
+    val resource_dir = new File(cwd, "src/main/resources")
+    val master_file = new File(resource_dir, "variables/master.yml")
+    // We usually want to reload rules during development,
+    // so we try to load them from the filesystem first, then jar.
+    if (master_file.exists()) {
+      // read file from filesystem
+      val rules = FileUtils.readFileToString(master_file, StandardCharsets.UTF_8)
+      // creates an extractor engine using the rules and the default actions
+      val extractor = ExtractorEngine(rules, path = Some(resource_dir))
+      new VariableProcessor(processor, extractor)
+    } else {
+      // read rules from yml file in resources
+      val source = io.Source.fromURL(getClass.getResource("/variables/master.yml"))
+      val rules = source.mkString
+      source.close()
+      // creates an extractor engine using the rules and the default actions
+      val extractor = ExtractorEngine(rules)
+      new VariableProcessor(processor, extractor)
+    }
+  }
+
 }
