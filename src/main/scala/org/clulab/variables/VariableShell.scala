e56685bcc5aed671cc4eb2388dad89cc95776a58
Keith Alcock <github@keithalcock.com>
2021-10-06 16:05:04 -0700

    Use the design from Marco, but make the menu locally
    This requires a the matching local update to processors

--- src/main/scala/org/clulab/variables/VariableShell.scala
+++ src/main/scala/org/clulab/variables/VariableShell.scala
@@ -3,33 +3,37 @@ package org.clulab.variables
 import org.clulab.utils._
 
 /** Interactive shell for variable reading */
-object VariableShell extends App {
+class VariableShell() extends Shell {
   println("Creating VariableProcessor...\n")
   private var vp: VariableProcessor = VariableProcessor()
 
-  def reload(menu: Menu, key: String): Boolean = {
+  def reload(): Unit = {
     println("Reloading VariableProcessor...")
     vp = VariableProcessor(vp.processor)
-    true
   }
 
-  def parse(menu: Menu, text: String): Boolean = {
+  override def work(text: String): Unit = {
     // the actual reading
     val (doc, mentions) = vp.parse(text)
 
     // debug display the mentions
     displayMentions(mentions, doc)
-    true
   }
 
-  val lineReader = new CliReader("(Habitus)>>> ", "user.home", ".habitusshellhistory")
-  val mainMenuItems = Seq(
-    new HelpMenuItem(":help", "show commands"),
-    new MainMenuItem(":reload", "reload the variable processor", reload),
-    new ExitMenuItem(":exit", "exit system")
-  )
-  val defaultMenuItem = new DefaultMenuItem(parse)
-  val menu = new Menu("Welcome to the Habitus Shell!", lineReader, mainMenuItems, defaultMenuItem)
+  override def mkMenu(): Menu = {
+    val lineReader = new CliReader("(Habitus)>>> ", "user.home", ".habitusshellhistory")
+    val mainMenuItems = Seq(
+      new HelpMenuItem(":help", "show commands"),
+      new SimpleMainMenuItem(":reload", "reload the variable processor", reload _),
+      new ExitMenuItem(":exit", "exit system")
+    )
+    val defaultMenuItem = new SafeDefaultMenuItem(work)
 
-  menu.run()
+    new Menu("Welcome to the Habitus Shell!", lineReader, mainMenuItems, defaultMenuItem)
+  }
+}
+
+object VariableShell extends App {
+  val sh = new VariableShell
+  sh.shell()
 }
