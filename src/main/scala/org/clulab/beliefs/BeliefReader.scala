d4813f039bbc24f831f2e29fd7fe0fc8d2b0895a
mithunpaul08 <mithunpaul08@gmail.com>
2021-11-15 10:14:22 -0700

    adding beliefreader

---
+++ src/main/scala/org/clulab/beliefs/BeliefReader.scala
@@ -0,0 +1,52 @@
+package org.clulab.beliefs
+
+import org.clulab.habitus.utils.{JsonPrinter,TsvPrinter}
+import org.clulab.utils.{FileUtils, StringUtils, ThreadUtils}
+import org.clulab.utils.Closer.AutoCloser
+import java.io.File
+
+
+object BeliefReader {
+
+  def main(args: Array[String]): Unit = {
+    val props = StringUtils.argsToMap(args)
+    val inputDir = props("in")
+    val outputDir = props("out")
+    val threads = props.get("threads").map(_.toInt).getOrElse(1)
+
+    run(inputDir, outputDir, threads)
+  }
+
+  def run(inputDir: String, outputDir: String, threads: Int) {
+    new File(outputDir).mkdir()
+    val tsvOutputFile = outputDir + "/mentions.tsv"
+    val jsonOutputFile = outputDir + "/mentions.json"
+
+    val vp = BeliefProcessor()
+    val files = FileUtils.findFiles(inputDir, ".txt")
+    val parFiles = if (threads > 1) ThreadUtils.parallelize(files, threads) else files
+
+    new JsonPrinter(jsonOutputFile).autoClose { jsonPrinter =>
+      new TsvPrinter(tsvOutputFile).autoClose { tsvPrinter =>
+        for (file <- parFiles) {
+          try {
+            val text = FileUtils.getTextFromFile(file)
+            val filename = StringUtils.afterLast(file.getName, '/')
+            println(s"going to parse input file: $filename")
+            val (doc, mentions) = vp.parse(text)
+
+
+            synchronized {
+              tsvPrinter.outputMentions(mentions, doc, _, filename);
+              jsonPrinter.outputMentions(mentions, doc, _, filename)
+            }
+          }
+          catch {
+            case e: Exception => e.printStackTrace()
+          }
+        }
+      }
+    }
+  }
+
+}
