8dd3fcc77b08181e67a1aa0704e310d8d4f2b45b
mithunpaul08 <mithunpaul08@gmail.com>
2021-11-29 23:41:19 -0700

    adding check for less than 50% are numbers.todo add test case

--- src/main/scala/org/clulab/beliefs/BeliefProcessor.scala
+++ src/main/scala/org/clulab/beliefs/BeliefProcessor.scala
@@ -73,11 +73,10 @@ class BeliefProcessor(val processor: Processor,
   private def checkNumberCount(mention: Mention): Boolean = {
     var numberCounter = 0
     for (word <- mention.sentenceObj.words) {
+      //todo: add for float also
       if (Try(word.toInt).isSuccess) {
-        if (Try(word.toFloat).isSuccess) {
           numberCounter = numberCounter + 1
         }
-      }
   }
   numberCounter > mention.sentenceObj.words.length / 2
 }
