31a4dde3b306230925c1cf1996884dcd0ec699c3
Maria Alexeeva <maxaalexeeva@gmail.com>
2022-01-09 19:37:37 -0700

    added interviews reader with a basic tsv writer

---
+++ src/main/scala/org/clulab/habitus/interviews/InterviewsReader.scala
@@ -0,0 +1,56 @@
+package org.clulab.habitus.interviews
+
+import org.clulab.utils.{FileUtils, StringUtils, ThreadUtils}
+
+import java.io.{File, PrintWriter}
+
+object InterviewsReader {
+
+  def main(args: Array[String]): Unit = {
+    val inputDir = ""
+    val outputDir = ""
+    val threads = 1
+
+    run(inputDir, outputDir, threads)
+  }
+
+  def run(inputDir: String, outputDir: String, threads: Int): Unit = {
+    new File(outputDir).mkdir()
+
+    val vp = InterviewsProcessor()
+    val files = FileUtils.findFiles(inputDir, ".txt")
+    val parFiles = if (threads > 1) ThreadUtils.parallelize(files, threads) else files
+    val pw = new PrintWriter(new File(outputDir + "/mentions.tsv"))
+    pw.write("filename\tmention type\tfound by\tsentence\tmention text\targs in all next columns (argType: argText)\n")
+    for (file <- parFiles) {
+      try {
+        val unfiltered = FileUtils.getTextFromFile(file)
+        // fixme: temporary, simple text cleanup
+        val text = unfiltered.replace("\n",
+          " ").replace("- ", "")
+        val filename = StringUtils.afterLast(file.getName, '/')
+        println(s"going to parse input file: $filename")
+        val (_, mentions) = vp.parse(text)
+        val contentMentions = mentions.filter(m => m.labels.contains("Event"))
+        for (m <- contentMentions) println("Men: " + m.label + " " + m.text)
+
+        for (m <- contentMentions) {
+          pw.write(s"${filename}\t${m.label}\t${m.foundBy}\t${m.sentenceObj.getSentenceText}\t${m.text}")
+          for (arg <- m.arguments) {
+            if (arg._2.nonEmpty) {
+              // multiple args of same type are "::"-separated
+              pw.write(s"\t${arg._1}:\t${arg._2.map(_.text.trim().replace("\t", "")).mkString("::")}")
+            }
+          }
+          pw.write("\n")
+        }
+
+      }
+      catch {
+        case e: Exception => e.printStackTrace()
+      }
+    }
+    pw.close()
+
+  }
+}
