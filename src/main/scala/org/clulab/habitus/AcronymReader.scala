6a97ee7bb1a2600ac86045d1dc28d2f625537808
Keith Alcock <github@keithalcock.com>
2022-04-28 09:25:29 -0700

    Clean code

--- src/main/scala/org/clulab/habitus/AcronymReader.scala
+++ src/main/scala/org/clulab/habitus/AcronymReader.scala
@@ -4,42 +4,50 @@ import org.clulab.habitus.utils.EqualityHashBag
 import org.clulab.processors.Processor
 import org.clulab.utils.FileUtils
 
-object CapitalsReader extends App {
+object AcronymReader extends App {
   val inputDir: String = args(0)
   val processor: HabitusProcessor = new HabitusProcessor(None)
   val files = FileUtils.findFiles(inputDir, ".txt")
 
-  def getAllUpperAcronyms(processor: Processor, text: String): Seq[(String, Int)] = {
-    val document = processor.mkDocument(text)
-    val wordsAndValids = document.sentences.flatMap { sentence =>
-      val valids = sentence.words.scanLeft(false) { case (lowerSeen, word) =>
-        lowerSeen || (word.head.isLetter && word.head.isLower)
-      }.drop(1)
+  trait Acronym {
+    def is(word: String): Boolean
+    def mk(word: String): String
+  }
 
-      sentence.words.zip(valids)
-    }
-    val validWords = wordsAndValids.filter(_._2).map(_._1)
-    val allUpperToWords = validWords.groupBy { word =>
+  object AllUpperAcronym extends Acronym {
+
+    def is(word: String): Boolean = {
       // This will skip words with non-letters.
       word.forall { letter => letter.isLetter && letter.isUpper }
     }
-    val allUpperBag = EqualityHashBag(allUpperToWords(true))
-    val notAllUpperBag = EqualityHashBag(allUpperToWords(false).map(_.toUpperCase))
-    val allUpperToCounts = allUpperBag.map { allUpper =>
-      allUpper -> (allUpperBag.count(allUpper), notAllUpperBag.count(allUpper))
-    }.toMap
-    val allUpperToGt2Eq0 = allUpperToCounts.filter { case (_, (allUpperCount, notAllUpperCount)) =>
-      allUpperCount > 1 && notAllUpperCount == 0
+
+    def mk(word: String): String = {
+      word.toUpperCase
     }
-
-    val allUpperAndCount = allUpperToGt2Eq0.map { case (allUpper, (allUpperCount, _)) =>
-      (allUpper, allUpperCount)
-    }.toSeq.sortBy { case (allUpper, allUpperCount) => (-allUpperCount, allUpper) }
-
-    allUpperAndCount
   }
 
-  def getInitialUpperAcronyms(processor: Processor, text: String): Seq[(String, Int)] = {
+  object OneUpperAcronym extends Acronym {
+
+    def is(word: String): Boolean = {
+      word.zipWithIndex.forall { case (char, index) =>
+        char.isLetter && ((index == 0 && char.isUpper) || (index != 0 && char.isLower))
+      }
+    }
+
+    def mk(word: String): String = {
+      val letters = word.zipWithIndex.map { case (char, index) =>
+        if (!char.isLetter)
+          char
+        else if (index == 0)
+          char.toUpper
+        else char.toLower
+      }
+
+      "" ++ letters
+    }
+  }
+
+  def getAcronyms(acronym: Acronym, processor: Processor, text: String): Seq[(String, Int)] = {
     val document = processor.mkDocument(text)
     val wordsAndValids = document.sentences.flatMap { sentence =>
       val valids = sentence.words.scanLeft(false) { case (lowerSeen, word) =>
@@ -49,44 +57,33 @@ object CapitalsReader extends App {
       sentence.words.zip(valids)
     }
     val validWords = wordsAndValids.filter(_._2).map(_._1)
-    val initialUpperWords = validWords.groupBy { word =>
-      // This will skip words with non-letters.
-      word.head.isLetter && word.head.isUpper &&
-      word.drop(1).forall { letter => letter.isLetter && letter.isLower }
-    }
-    val initialUpperBag = EqualityHashBag(initialUpperWords(true))
-    val notInitialUpperBag = EqualityHashBag(initialUpperWords(false).map { word =>
-      val letters = word.zipWithIndex.map { case (letter, index) =>
-        if (index == 0) letter.toUpper
-        else letter.toLower
-      }
-
-      "" ++ letters
-    })
-    val allUpperToCounts = initialUpperBag.map { allUpper =>
-      allUpper -> (initialUpperBag.count(allUpper), notInitialUpperBag.count(allUpper))
+    val isToWords = validWords.groupBy(acronym.is)
+    val trueBag = EqualityHashBag(isToWords(true))
+    val falseBag = EqualityHashBag(isToWords(false).map(acronym.mk))
+    val countMap = trueBag.map { word =>
+      word -> (trueBag.count(word), falseBag.count(word))
     }.toMap
-    val allUpperToGt2Eq0 = allUpperToCounts.filter { case (_, (allUpperCount, notAllUpperCount)) =>
-      allUpperCount > 1 && notAllUpperCount == 0
+    val goodCountMap = countMap.filter { case (_, (trueCount, falseCount)) =>
+      trueCount > 1 && falseCount == 0
     }
+    val goodCountSeq = goodCountMap
+        .map { case (word, (trueCount, _)) => (word, trueCount) }
+        .toSeq
+        .sortBy { case (word, trueCount) => (-trueCount, word) }
 
-    val allUpperAndCount = allUpperToGt2Eq0.map { case (allUpper, (allUpperCount, _)) =>
-      (allUpper, allUpperCount)
-    }.toSeq.sortBy { case (allUpper, allUpperCount) => (-allUpperCount, allUpper) }
-
-    allUpperAndCount
+    goodCountSeq
   }
 
   files.foreach { file =>
     val text = FileUtils.getTextFromFile(file)
-    val allUpperAndCount = getAllUpperAcronyms(processor, text)
-    val initialUpperAndCount = getInitialUpperAcronyms(processor, text)
+    val allUpperAndCount = getAcronyms(AllUpperAcronym, processor, text)
+    val oneUpperAndCount = getAcronyms(OneUpperAcronym, processor, text)
 
     println(s"\nFile: ${file.getName}\n")
     allUpperAndCount.foreach { case (allUpper, count) =>
       println(s"$allUpper\t$count")
     }
-    initialUpperAndCount.foreach { case (allUpper, count) =>
+    oneUpperAndCount.foreach { case (allUpper, count) =>
       println(s"$allUpper\t$count")
     }
   }
