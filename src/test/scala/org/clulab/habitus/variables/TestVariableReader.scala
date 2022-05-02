63b00d8d08753cbdb9e1d07ecd26649557ce8bc7
Maria Alexeeva <maxaalexeeva@gmail.com>
2022-05-02 14:56:13 -0700

    adjusting test labels

--- src/test/scala/org/clulab/habitus/variables/TestVariableReader.scala
+++ src/test/scala/org/clulab/habitus/variables/TestVariableReader.scala
@@ -27,7 +27,6 @@ class TestVariableReader extends Test {
         if (index == -1)
           println("Put a breakpoint here to observe a particular test.")
 
-//        val mentions = getMentions(text).filter(_.label matches "Assignment")
         val mentions = getMentions(text).filter(_.label matches label)
         if (label == "None") {
           // get only relations and events
@@ -61,7 +60,7 @@ class TestVariableReader extends Test {
     // The CLU parser breaks on this one, but the SRL works fine!
     VariableTest(
       "sent1", "Farmers’ sowing dates ranged from 14 to 31 July for the WS and from 3 to 11 March for the DS.",
-      "SowingDate",
+      "PlantingDate",
       Seq(
         ("sowing dates", Seq(("from 3 to 11 March", "XXXX-03-03 -- XXXX-03-11"))),
         ("sowing dates", Seq(("from 14 to 31 July", "XXXX-07-14 -- XXXX-07-31")))
@@ -69,17 +68,17 @@ class TestVariableReader extends Test {
     ),
     VariableTest(
       "sent2", "Sowing between October 4 and October 14 was optimal.",
-      "SowingDate",
+      "PlantingDate",
       Seq(("Sowing", Seq(("between October 4 and October 14", "XXXX-10-04 -- XXXX-10-14"))))
     ),
     VariableTest(
       "sent3", "Sowing date was October 7, 2019 .",
-      "SowingDate",
+      "PlantingDate",
       Seq(("Sowing date", Seq(("October 7, 2019", "2019-10-07"))))
     ),
     VariableTest(
       "sent4", "Sowing between October 4 and October 14 of 2020 was optimal.",
-      "SowingDate",
+      "PlantingDate",
       Seq(("Sowing", Seq(("between October 4 and October 14 of 2020", "2020-10-04 -- 2020-10-14"))))
     ),
     VariableTest(
@@ -95,7 +94,7 @@ class TestVariableReader extends Test {
     VariableTest(
       "sent7", "Planting occurred on October 2.",
       "PlantingDate",
-      Seq(("Planting", Seq(("October 2", "XXXX-10-02"))))
+      Seq(("PlantingEvent", Seq(("October 2", "XXXX-10-02"))))
     ),
     VariableTest(
       "sent8", "Planting date was October 4, 2019",
@@ -109,17 +108,17 @@ class TestVariableReader extends Test {
     ),
     VariableTest(
       "sent10", "Sowing occurred on October 1, 2019",
-      "SowingDate",
+      "PlantingDate",
       Seq(("Sowing", Seq(("October 1, 2019", "2019-10-01"))))
     ),
     VariableTest(
       "sent10_2", "Seeding occurred on October 1, 2019",
-      "SowingDate",
+      "PlantingDate",
       Seq(("Seeding", Seq(("October 1, 2019", "2019-10-01"))))
     ),
     VariableTest(
       "sent11", "Sowing on October 9, 2019.",
-      "SowingDate",
+      "PlantingDate",
       Seq(("Sowing", Seq(("October 9, 2019", "2019-10-09"))))
     ),
     VariableTest(
@@ -129,22 +128,22 @@ class TestVariableReader extends Test {
     ),
     VariableTest(
       "sent12", "Sowing between October 4 and October 14 of 2020 was optimal.",
-      "SowingDate",
+      "PlantingDate",
       Seq(("Sowing", Seq(("between October 4 and October 14 of 2020", "2020-10-04 -- 2020-10-14"))))
     ),
     VariableTest(
       "sent12_1", "Sowing dates between October 4 and October 14, 2020 was optimal.",
-      "SowingDate",
+      "PlantingDate",
       Seq(("Sowing dates", Seq(("between October 4 and October 14, 2020", "2020-10-04 -- 2020-10-14"))))
     ),
     VariableTest(
       "sent13", "Sowing in May, 2019.",
-      "SowingDate",
+      "PlantingDate",
       Seq(("Sowing", Seq(("May, 2019", "2019-05-XX"))))
     ),
     VariableTest(
       "sent14", "Sowing in April, 2011 could increase outputs.",
-      "SowingDate",
+      "PlantingDate",
       Seq(("Sowing", Seq(("April, 2011", "2011-04-XX"))))
     ),
     VariableTest(
@@ -154,23 +153,23 @@ class TestVariableReader extends Test {
     ),
     VariableTest(
       "sent15", "Sowing on September 8 yields best outcomes.",
-      "SowingDate",
+      "PlantingDate",
       Seq(("Sowing", Seq(("September 8", "XXXX-09-08"))))
     ),
     //TODO: ms, but without [, year] it does not complain.
     VariableTest(
       "sent15_2", "Sowing on April 8 yields best outcomes.",
-      "SowingDate",
+      "PlantingDate",
       Seq(("Sowing", Seq(("April 8", "XXXX-04-08"))))
     ),
     VariableTest(
       "sent15_3", "Sowing on April 7, 2001 yields best outcomes.",
-      "SowingDate",
+      "PlantingDate",
       Seq(("Sowing", Seq(("April 7, 2001", "2001-04-07"))))
     ),
     VariableTest(
       "sent15_4", "The first sowing dates started on July 1st in 2010 and on July 8th in 2011",
-      "SowingDate",
+      "PlantingDate",
       Seq(
         ("sowing dates", Seq(("July 1st", "XXXX-07-01"))),
         ("sowing dates", Seq(("2010", "2010-XX-XX"))),
@@ -180,27 +179,27 @@ class TestVariableReader extends Test {
     ),
     VariableTest(
       "sent15_5", "sowing ( July 15 )",
-      "SowingDate",
+      "PlantingDate",
       Seq(("sowing", Seq(("July 15", "XXXX-07-15"))))
     ),
     VariableTest(
       "sent15_6", "Early sowing (before July 15)",
-      "SowingDate",
+      "PlantingDate",
       Seq(("Early sowing", Seq(("before July 15", "XXXX-XX-XX -- XXXX-07-15"))))
     ),
     VariableTest(
       "sent15_7", "Late sowings (beyond August 15) total 82% achievements either 19,762 ha.",
-      "SowingDate",
+      "PlantingDate",
       Seq(("Late sowings", Seq(("beyond August 15", "XXXX-08-15 -- XXXX-XX-XX"))))
     ),
     VariableTest(
       "sent16_7", "sowing was done on 25th October",
-      "SowingDate",
+      "PlantingDate",
       Seq(("sowing", Seq(("25th October", "XXXX-10-25"))))
     ),
     VariableTest(
       "sent16_8", "sowing date began on July 8th",
-      "SowingDate",
+      "PlantingDate",
       Seq(("sowing date", Seq(("July 8th", "XXXX-07-08"))))
     ),
     VariableTest(
@@ -210,7 +209,7 @@ class TestVariableReader extends Test {
     ),
     VariableTest(
       "sent16_10", "Sowing after July 15 th reduced aerial biomass and grain yield",
-      "SowingDate",
+      "PlantingDate",
       Seq(("Sowing", Seq(("after July 15", "XXXX-07-15 -- XXXX-XX-XX"))))
     ),
     // Tests for CULTIVARS
@@ -219,61 +218,61 @@ class TestVariableReader extends Test {
     // the lexicon, e.g., buckwheat (same thing for all crop and fertilizer entity-like tests in this file)
     VariableTest(
       "sent16", "The most important planted cash crop is peanut in the SRV.",
-      "Assignment",
+      "CropAssignment",
       Seq(("crop", Seq(("peanut", ""))))
     ),
     // todo: move to entity test, just check if crops are extracted as Crop
     VariableTest(
       "sent16_0", "Most farmers in the SRV plant Sahel 108",
-      "Assignment",
+      "CropAssignment",
       Seq(("plant", Seq(("Sahel 108", ""))))
     ),
     // todo: move to entity test, just check if crops are extracted as Crop
     VariableTest(
       "sent16_1", "Some farmers use variety like Sahel 108",
-      "Assignment",
+      "CropAssignment",
       Seq(("variety", Seq(("Sahel 108", ""))))
     ),
     // todo: move to entity test, just check if crops are extracted as Crop
     VariableTest(
       "sent16_2", "The productivity of a range of agricultural crops beyond rice",
-      "Assignment",
+      "CropAssignment",
       Seq(("crops", Seq(("rice", ""))))
     ),
     // todo: move to entity test, just check if crops are extracted as Crop
     VariableTest(
       "sent16_3", "In the SRV where irrigated rice is the most common grown crop",
-      "Assignment",
+      "CropAssignment",
       Seq(("crop", Seq(("rice", ""))))
     ),
     // todo: move to entity test, just check if crops are extracted as Crop
     VariableTest(
       "sent16_2_1", "Farmers use cultivar such as sugarcane which are planted on 80 and 20 percent of total area",
-      "Assignment",
+      "CropAssignment",
       Seq(("cultivar", Seq(("sugarcane", ""))))
     ),
     // todo: move to entity test, just check if crops are extracted as Crop
     VariableTest(
       "sent16_2_2", "AfricaRice, a CGIAR research center, developed the seed Sahel 108",
-      "Assignment",
+      "CropAssignment",
       Seq(("seed", Seq(("Sahel 108", ""))))
     ),
     // todo: move to entity test, just check if crops are extracted as Crop
     VariableTest(
       "sent16_2_3", "Farmers preferred to use short duration rice varieties like Sahel 108",
-      "Assignment",
+      "CropAssignment",
       Seq(("varieties", Seq(("Sahel 108", ""))))
     ),
     // todo: move to entity test, just check if crops are extracted as Crop
     VariableTest(
       "sent16_2_4", "Other crops cultivated include millet",
-      "Assignment",
+      "CropAssignment",
       Seq(("crops", Seq(("millet", ""))))
     ),
     // todo: move to entity test, just check if crops are extracted as Crop
     VariableTest(
       "sent16_3_1", "Other crops cultivated include millet, sorghum, maize, cowpea and vegetables",
-      "Assignment",
+      "CropAssignment",
       Seq(
         ("crops", Seq(("millet", ""))),
         ("crops", Seq(("sorghum", ""))),
@@ -284,24 +283,24 @@ class TestVariableReader extends Test {
     ),
     VariableTest(
       "sent16_4", "In the 1998WS, farmers sowed Jaya between 20 June and 1 July",
-      "Planting",
+      "PlantingEvent",
       // The second one of these wasn't initially tested.
       Seq(("sowed",Seq(("Jaya", ""), ("between 20 June and 1 July", "XXXX-06-20 -- XXXX-07-01"))))
     ),
     VariableTest(
       "birdattack_1", "Bird attacks occurred between July 1st and August 31st",
-      "BirdAttack",
+      "BirdAttackEvent",
       Seq(("Bird attacks", Seq(("between July 1st and August 31st", "XXXX-07-01 -- XXXX-08-31"))))
     ),
     // todo: should be entity test for groundnut (but if we are checking if crop was extracted through event rule, use a crop that's not in the lexicon, e.g. buckwheat)
     VariableTest(
       "sent16_4_1", "They chose furthermore to grow only one cultivar groundnut",
-      "Assignment",
+      "CropAssignment",
       Seq(("cultivar", Seq(("groundnut", ""))))
     ),
     VariableTest(
       "sent16_4_2", "In the SRV, farmers plant Sahel 108, Sahel 150, Sahel 154, Sahel 134, Nerica",
-      "Planting",
+      "PlantingEvent",
       Seq(("plant",
         Seq(("Sahel 108", ""), ("Sahel 150", ""), ("Sahel 154", ""), ("Sahel 134", ""), ("Nerica", ""))
       ))
@@ -309,7 +308,7 @@ class TestVariableReader extends Test {
     // todo: make into entity rule with unusual crops
     VariableTest(
       "sent17_1", "Peanut, sugarcane and cotton are important cash crops.",
-      "Assignment",
+      "CropAssignment",
       Seq(
         ("crops", Seq(("Peanut", ""))),
         ("crops", Seq(("sugarcane", ""))),
@@ -319,7 +318,7 @@ class TestVariableReader extends Test {
     // todo: make into entity rule with unusual crops
     VariableTest(
       "sent17_2", "Millet, rice, corn and sorghum are the primary food crops grown in Senegal.",
-      "Assignment",
+      "CropAssignment",
       Seq(
         ("crops", Seq(("Millet", ""))),
         ("crops", Seq(("rice", ""))),
@@ -330,7 +329,7 @@ class TestVariableReader extends Test {
     // todo: make into entity rule with unusual crops
     VariableTest(
       "sent17_3", "Tomato or onion were the two most labour-consuming crops",
-      "Assignment",
+      "CropAssignment",
       Seq(
         ("crops", Seq(("Tomato", ""))),
         ("crops", Seq(("onion", "")))
@@ -339,7 +338,7 @@ class TestVariableReader extends Test {
     // todo: make into entity rule with unusual crops
     VariableTest(
       "sent17_4", "Onion and tomato were the most profitable crops.",
-      "Assignment",
+      "CropAssignment",
       Seq(
         ("crops", Seq(("Onion", ""))),
         ("crops", Seq(("tomato", "")))
@@ -349,49 +348,49 @@ class TestVariableReader extends Test {
     // todo: test for Fertilizer Entity
     VariableTest(
       "sent20", "One of the most important farming input is mineral fertilizer",
-      "Assignment",
+      "FertilizerAssignment",
       Seq(("input", Seq(("mineral", ""))))
     ),
     // todo: test for Fertilizer Entity
     VariableTest(
       "sent20_1", "Fertilizer nitrogen (N) has been applied at two or more levels",
-      "Assignment",
+      "FertilizerAssignment",
       Seq(("Fertilizer", Seq(("nitrogen", ""))))
     ),
     // todo: test for Fertilizer Entity
     VariableTest(
       "sent20_2", "In fact, use of fertilizer P has declined steadily since 1995",
-      "Assignment",
+      "FertilizerAssignment",
       Seq(("fertilizer", Seq(("P", ""))))
     ),
     // todo: test for Fertilizer Entity
     VariableTest(
       "sent20_3", "The amount of fertilizer N required was averaged at 52 kg ha-1",
-      "Assignment",
+      "FertilizerAssignment",
       Seq(("fertilizer", Seq(("N", ""))))
     ),
     // todo: test for Fertilizer Entity
     VariableTest(
       "sent20_4", "The most widely used solid inorganic fertilizers are urea, diammonium phosphate and potassium chloride",
-      "Assignment",
+      "FertilizerAssignment",
       Seq(("fertilizers", Seq(("urea", ""), ("diammonium phosphate", ""), ("potassium chloride", ""))))
     ),
     // todo: test for Fertilizer Entity
     VariableTest(
       "sent20_5", "The most widely used solid inorganic fertilizers are diammonium phosphate, urea and potassium chloride",
-      "Assignment",
+      "FertilizerAssignment",
       Seq(("fertilizers", Seq(("diammonium phosphate", ""), ("urea", ""), ("potassium chloride", ""))))
     ),
     // todo: test for Fertilizer Entity
     VariableTest(
       "sent20_5_1", "The main nutrient elements present in the fertilizers are nitrogen, phosphorus, and potassium",
-      "Assignment",
+      "FertilizerAssignment",
       Seq(("fertilizers", Seq(("nitrogen", ""), ("phosphorus", ""), ("potassium", ""))))
     ),
     // todo: test for Fertilizer Entity
     VariableTest(
       "sent20_5_2", "The nitrogenous chemical fertilizers are urea, calcium, ammonium nitrate, ammonium sulfate, basic calcium nitrate, calcium cyanamide",
-      "Assignment",
+      "FertilizerAssignment",
       Seq(("fertilizers", Seq(("urea", ""), ("calcium", ""), ("ammonium nitrate", ""), ("ammonium sulfate", ""), ("basic calcium nitrate", ""), ("calcium cyanamide", ""))))
     ),
     VariableTest(
@@ -402,13 +401,13 @@ class TestVariableReader extends Test {
     // todo: entity test
     VariableTest(
       "sent20_7", "However, the most unvalable fertilizer was diammonium-phosphate",
-      "Assignment",
+      "FertilizerAssignment",
       Seq(("fertilizer", Seq(("diammonium-phosphate", ""))))
     ),
     // todo: entity test
     VariableTest(
       "sent20_8", "Phosphorus, potassium and NPK are important inorganic fertilizers.",
-      "Assignment",
+      "FertilizerAssignment",
       Seq(
         ("fertilizers", Seq(("Phosphorus", ""))),
         ("fertilizers", Seq(("potassium", ""))),
@@ -418,7 +417,7 @@ class TestVariableReader extends Test {
     // todo: entity test
     VariableTest(
       "sent20_9", "Some organic fertilizers include nitrogen, phosphorus, and potassium as the three most important elements for plant nutrition",
-      "Assignment",
+      "FertilizerAssignment",
       Seq(
         ("fertilizers", Seq(("nitrogen", ""))),
         ("fertilizers", Seq(("phosphorus", ""))),
@@ -428,38 +427,38 @@ class TestVariableReader extends Test {
     // Tests for sentences found in SAED bulletins Dec 2021
     VariableTest(
       "sent21_1", "Sowing (15 July - 15 August) concentrates 20% of the sown areas, or 4,239 ha;",
-      "SowingDate",
+      "PlantingDate",
       Seq(("Sowing", Seq(("15 July - 15 August", "XXXX-07-15 -- XXXX-08-15"))))
     ),
     VariableTest(
       "sent21_2", " Early sowing (before July 15) covers 1% of the total areas developed, ie 242 ha.",
-      "SowingDate",
+      "PlantingDate",
       Seq(("Early sowing", Seq(("before July 15", "XXXX-XX-XX -- XXXX-07-15"))))
     ),
     VariableTest(
       "sent21_3", "Late sowing (beyond August 15) accounts for 79% of the projects, ie 17,043 ha.",
-      "SowingDate",
+      "PlantingDate",
       Seq(("Late sowing", Seq(("beyond August 15", "XXXX-08-15 -- XXXX-XX-XX"))))
     ),
     VariableTest(
       "sent21_4", "early sowing (month of January) occupies a small proportion of cultivated areas.",
-      "SowingDate",
+      "PlantingDate",
       Seq(("early sowing", Seq(("month of January", "XXXX-01-XX"))))
     ),
     VariableTest(
       "sent21_5", "Thus, the rate of sowing intensified beyond February 25, 2020 with a marked increase in the areas sown, particularly in Dagana.",
-      "SowingDate",
+      "PlantingDate",
       Seq(("sowing", Seq(("beyond February 25, 2020", "2020-02-25 -- XXXX-XX-XX"))))
     ),
     VariableTest(
       "sent21_6", "The late start of the campaign, due among other constraints to the delay in holding the 1er credit committee, has had an impact on the crop calendar, in particular with so-called late sowing (beyond March 17) which takes up a large part of the development",
-      "SowingDate",
+      "PlantingDate",
       Seq(("late sowing", Seq(("beyond March 17", "XXXX-03-17 -- XXXX-XX-XX"))))
     ),
     // FIXME: "before mid-July" is tokenized weird
     VariableTest(
       "sent21_7", "Overall, it is noted that 65% of the areas are developed beyond September 15, 2020 (late sowing), the areas sown during the recommended period (between July 15 and August 15) cover 34% of the plantings and early sowing (before mid-July) represents 1% of the total development.",
-      "SowingDate",
+      "PlantingDate",
       Seq(
         ("late sowing", Seq(("beyond September 15, 2020", "2020-09-15 -- XXXX-XX-XX"))),
         ("sown", Seq(("between July 15 and August 15", "XXXX-07-15 -- XXXX-08-15"))),
@@ -468,17 +467,17 @@ class TestVariableReader extends Test {
     ),
     VariableTest(
       "sent21_8", "2% of the areas currently cultivated are sown before February 15, 2020, i.e. 783 ha ;",
-      "SowingDate",
+      "PlantingDate",
       Seq(("sown", Seq(("before February 15, 2020", "XXXX-XX-XX -- 2020-02-15"))))
     ),
     VariableTest(
       "sent21_9", "In fact, early sowing (month of January) occupies a small proportion of cultivated areas.",
-      "SowingDate",
+      "PlantingDate",
       Seq(("early sowing", Seq(("month of January", "XXXX-01-XX"))))
     ),
     VariableTest(
       "sent21_10", "To date , the distribution according to the cropping calendar , of the sowing carried out at the level of the Dagana delegation is as follows : - - - 9 % of the areas currently cultivated are sown before February 15 , 2020 , i.e. 3,146.92 ha ; Between the date of February 15 to March 15 , 2020 , are sown 66 % areas developed , i.e. 21,900.73 ha ; Areas sown beyond March 15 , 2020 cover 25 % of the entire development , i.e. 8,278.18 ha",
-      "SowingDate",
+      "PlantingDate",
       Seq(
         ("sown", Seq(("before February 15 , 2020", "XXXX-XX-XX -- 2020-02-15"))),
         ("sown", Seq(("February 15 to March 15 , 2020", "2020-02-15 -- 2020-03-15"))),
