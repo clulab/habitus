1ee069d2cb7151cc625b3a805198ca1ee179d17f
Isaac Kobby Anni <annqwabena@gmail.com>
2022-05-03 16:16:59 +0000

    test edited

--- src/test/scala/org/clulab/habitus/variables/EntitiesTest.scala
+++ src/test/scala/org/clulab/habitus/variables/EntitiesTest.scala
@@ -3,6 +3,8 @@ package org.clulab.habitus.variables
 import org.clulab.habitus.utils.Test
 import org.clulab.odin.Mention
 
+import scala.collection.mutable.ArrayBuffer
+
 class EntitiesTest extends Test {
   // checks entity mentions:
   // the variable argument should be a text bound mention of the correct label
@@ -16,7 +18,7 @@ class EntitiesTest extends Test {
                            // Expected label extraction; a text can have multiple extractions for multiple labels.
                            labelExpected: Array[String],
                            // Expected values from text extraction.
-                           expectedTextValue: Array[String]
+                           requiredSize: Array[Int]
                          ) {
 
     def getMentions(text: String): Seq[Mention] = {
@@ -35,16 +37,43 @@ class EntitiesTest extends Test {
         val mentions = getMentions(text)
 
         // filter mentions by expected labels
-        for (label <- labelExpected){
-          val targetMentions =  mentions.filter(_.label matches  label)
-          targetMentions should have size (1)
+//        for (label <- labelExpected){
+//          val targetMentions =  mentions.filter(_.label matches  label)
+//          targetMentions should have size (1)
+//        }
+        // zip label with expected sizes.
+        val labelTextSize = labelExpected zip requiredSize
+
+        for ( (label, size) <- labelTextSize){
+          // Get an empty list
+          val newMentions = new ArrayBuffer[String]()
+
+          // loop through all mentions and map text to required labels
+          // append the filtered text
+          for (textMention <- mentions){
+            if (textMention.label == label)
+              newMentions.append(textMention.text)
+          }
+
+          // check size of the text values
+          newMentions should have size (size)
+          // check existence of required text in newMention
+          newMentions.foreach(text => newMentions should contain(text))
         }
+//        val mentionText = mentions.map(_.text)
+//        val mentionText = mentions.map(m => m.text)
+//        val newMentions = new ArrayBuffer[String]()
+//        for (m <- mentions){
+//          if (m.label == "Crop")
+//            newMentions.append(m.text)
+//        }
+
         // I think I would need a better way to map ONLY extracted values. For now, here is what I am able to do.
         // I do not know how to use "append" like in python, I could have append ONLY extracted
         // entities to `targetMentionsLabelTexts` from the `for loop above` and check for their existence in the last line.
-        val targetMentionsLabelTexts = mentions.map(_.text)
-        val desiredLabelTexts = expectedTextValue
-        desiredLabelTexts.foreach(text => targetMentionsLabelTexts should contain(text))
+//        val targetMentionsLabelTexts = mentions.map(_.text)
+//        val desiredLabelTexts = expectedTextValue
+//        desiredLabelTexts.foreach(text => targetMentionsLabelTexts should contain(text))
 
       }
     }
@@ -55,27 +84,27 @@ class EntitiesTest extends Test {
   val variableTests: Array[VariableTest] = Array(
     VariableTest(
       "sent1",
-      "with an average yield over years and seasons of 5 t ha-1",
-      Array("Yield"),
-      Array("yield")
+      "with an average yield over years yields and seasons of 5 t ha-1",
+      Array("Yield", "Value"),
+      Array(2, 1)
     ),
     VariableTest(
       "sent2",
       "The potential grain yield that can be obtained ranges from 8 to 9 t ha-1 in the wet season (July sowing) and from 6 to 11 t ha-1 in the dry season (February sowing)",
       Array("Yield","WetSeason", "DrySeason"),
-      Array("yield", "wet season", "dry season")
+      Array(1, 1, 1)
     ),
     VariableTest(
       "sent3",
       "Double cropping (growing rice in the wet season and dry season on the same field) is possible.",
       Array("WetSeason", "DrySeason"),
-      Array("wet season", "dry season")
+      Array(1, 1)
     ),
     VariableTest(
       "sent4",
       "irrigation rules resulted in great variability of irrigation frequency between fields, and sub-optimal timing of nitrogen fertilizer application resulted in yield losses",
       Array("Yield"),
-      Array("yield")
+      Array(1)
     ),
 
 
