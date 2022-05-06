aba90e8581d922f03a0edc0e6e5bc1038dba1226
Keith Alcock <github@keithalcock.com>
2022-05-06 08:22:24 -0700

    Account for empty words

--- src/main/scala/org/clulab/habitus/apps/utils/WordTypes.scala
+++ src/main/scala/org/clulab/habitus/apps/utils/WordTypes.scala
@@ -5,14 +5,12 @@ object WordTypes extends Enumeration {
   val AllLower, AllUpper, InitialUpper, NonWord, Other = Value
 
   def apply(word: String): WordType = {
-    require(word.nonEmpty)
-
     val letterCount = word.count(_.isLetter)
     // isLower and isUpper on non-letters will both be false.
     val lowerCount = word.count(_.isLower)
     val upperCount = word.count(_.isUpper)
 
-    if (letterCount == 0)
+    if (letterCount == 0) // This also accounts for word.isEmpty.
       NonWord
     else if (lowerCount == letterCount)
       AllLower
