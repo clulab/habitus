6982919c8a71400f175ef5134080007420a92f87
Hubert Kanyamahanga <hubakanye20@gmail.com>
2022-05-07 11:56:45 +0000

    Added class for multiple files ner extractions

--- src/main/scala/org/clulab/habitus/apps/utils/WordTypes.scala
+++ src/main/scala/org/clulab/habitus/apps/utils/WordTypes.scala
@@ -5,10 +5,14 @@ object WordTypes extends Enumeration {
   val AllLower, AllUpper, InitialUpper, NonWord, Other = Value
 
   def apply(word: String): WordType = {
+    require(word.nonEmpty)
+
     val letterCount = word.count(_.isLetter)
     // isLower and isUpper on non-letters will both be false.
     val lowerCount = word.count(_.isLower)
     val upperCount = word.count(_.isUpper)
+    if (word.isEmpty)
+      println(word)
 
     if (letterCount == 0) // This also accounts for word.isEmpty.
       NonWord
