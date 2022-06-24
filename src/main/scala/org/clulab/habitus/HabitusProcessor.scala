6e51215daae3926da060d811c3a11c1b12466f74
Keith Alcock <github@keithalcock.com>
2022-06-23 20:01:52 -0700

    Do floatsInARow
    Rewrite alpha function as well

--- src/main/scala/org/clulab/habitus/HabitusProcessor.scala
+++ src/main/scala/org/clulab/habitus/HabitusProcessor.scala
@@ -67,15 +67,28 @@ class HabitusProcessor(lexiconNer: Option[LexiconNER], filter: Boolean = true) e
     mostInARow(sentence.raw, string => 1 <= string.length && string.length <= 3)
   }
 
+  def mostFloatsInARow(sentence: Sentence): Int = {
+    // This could be sentence.words instead of raw.
+    // Make sure there is at least one digit, even leading 0s, and then only
+    // more digits or decimal points, even if there are multiple decimal points.
+    // Integers are a subset of floats.  The decimal point isn't required.
+    mostInARow(sentence.raw, string => {
+      string.exists("123456789".contains(_)) &&
+      string.forall("0123456789.".contains(_))
+    })
+  }
+
   /** Returns true if this is a malformed sentence
     * malformed= either > 150 tokens or
     * more than 50% of tokens are numbers */
   def isBadSentence(sentence: Sentence): Boolean = {
+    val length = sentence.words.length
     val isBad =
-        !HabitusProcessor.wordCountRange.contains(sentence.words.length) ||
-        HabitusProcessor.nonAlphaLimit < getNonAlphaCount(sentence).toDouble / sentence.words.length ||
+        !HabitusProcessor.wordCountRange.contains(length) ||
+        HabitusProcessor.nonAlphaCountLimit(length) < getNonAlphaCount(sentence) ||
         HabitusProcessor.singleLetterLimit < mostSingleLettersInARow(sentence) ||
-        HabitusProcessor.customKernLimit < mostCustomKernsInARow(sentence)
+        HabitusProcessor.customKernLimit < mostCustomKernsInARow(sentence) ||
+        HabitusProcessor.floatLimit < mostFloatsInARow(sentence)
     // println(s"isBad = $isBad")
     isBad
   }
@@ -83,7 +96,19 @@ class HabitusProcessor(lexiconNer: Option[LexiconNER], filter: Boolean = true) e
 
 object HabitusProcessor {
   val wordCountRange = Range.inclusive(3, 150)
-  val nonAlphaLimit = 0.45
+  val nonAlphaLimit = 0.50
   val singleLetterLimit = 15
   val customKernLimit = 15
+  val floatLimit = 5
+
+  def nonAlphaCountLimit(length: Int): Int = {
+    // The integer arithmetic used here originally made use of truncation.
+    // For positive values, that's the same as floor.
+    // The condition should be specifiable as limit < function(sentence)
+    // with limit a float/double so it has been rewritten.
+    // getAlphaCount(sentence) < sentence.words.length / 2 ||
+    // getAlphaCount(sentence) < math.floor(sentence.words.length * alphaLimit) ||
+    // math.ceil(sentence.words.length * nonAlphaLimit) < getNonAlphaCount(sentence)
+    math.ceil(length * nonAlphaLimit).toInt
+  }
 }
