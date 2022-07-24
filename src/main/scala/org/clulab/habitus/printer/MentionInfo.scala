1e4249302d8e2daa0b7d91d79c304d16aaf7b59d
Maria Alexeeva <maxaalexeeva@gmail.com>
2022-07-23 23:58:07 -0700

    added pseudo-paragraph (+/-3 sentence window) to tsv output; this is the version that produced extractions tsv sent to vj

--- src/main/scala/org/clulab/habitus/printer/MentionInfo.scala
+++ src/main/scala/org/clulab/habitus/printer/MentionInfo.scala
@@ -3,15 +3,18 @@ package org.clulab.habitus.printer
 import org.clulab.habitus.utils.Pairable
 import org.clulab.odin.Mention
 
-case class MentionInfo(sentenceText: String, inputFilename: String, label: String) extends Pairable
+case class MentionInfo(contextWindow: String, sentenceText: String, inputFilename: String, label: String) extends Pairable
 
 object MentionInfo {
 
   def apply(mention: Mention, inputFilename: String): MentionInfo = {
+    val windowSize = 3
     val document = mention.document
+    val sentInterval = (math.max(mention.sentence - windowSize, 0), math.min(mention.sentence + windowSize, mention.document.sentences.length + 1))
+    val contextWindow = mention.document.sentences.slice(sentInterval._1, sentInterval._2).map(_.getSentenceText).mkString(" ")
     val sentenceText = document.sentences(mention.sentence).getSentenceText
     val label = mention.label
-    new MentionInfo(sentenceText, inputFilename, label)
+    new MentionInfo(contextWindow, sentenceText, inputFilename, label)
   }
 }
 
