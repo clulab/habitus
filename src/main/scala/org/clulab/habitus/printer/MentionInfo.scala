2e54c4e24d02afe9a555a76d2179b4adcbed7802
Maria Alexeeva <maxaalexeeva@gmail.com>
2022-07-28 11:12:24 -0700

    using kwalcock's suggestion to simplify the code

--- src/main/scala/org/clulab/habitus/printer/MentionInfo.scala
+++ src/main/scala/org/clulab/habitus/printer/MentionInfo.scala
@@ -4,6 +4,7 @@ import com.typesafe.config.ConfigFactory
 import ai.lum.common.ConfigUtils._
 import org.clulab.habitus.utils.Pairable
 import org.clulab.odin.Mention
+import org.clulab.struct.Interval
 
 case class MentionInfo(contextWindow: String, sentenceText: String, inputFilename: String, label: String) extends Pairable
 
@@ -14,8 +15,10 @@ object MentionInfo {
 
   def apply(mention: Mention, inputFilename: String): MentionInfo = {
     val document = mention.document
-    val sentInterval = (math.max(mention.sentence - windowSize, 0), math.min(mention.sentence + windowSize + 1, mention.document.sentences.length))
-    val contextWindow = mention.document.sentences.slice(sentInterval._1, sentInterval._2).map(_.getSentenceText).mkString(" ")
+    val sentences = mention.document.sentences
+    val mentionRange = Interval(mention.sentence).indices
+    val sentRange = Range(mentionRange.start - windowSize, mentionRange.end + windowSize).intersect(sentences.indices)
+    val contextWindow = sentRange.map(sentences(_).getSentenceText).mkString(" ")
     val sentenceText = document.sentences(mention.sentence).getSentenceText
     val label = mention.label
     new MentionInfo(contextWindow, sentenceText, inputFilename, label)
