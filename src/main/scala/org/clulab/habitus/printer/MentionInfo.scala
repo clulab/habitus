6ef648f8c010bbc77bcc88ce100d8409025912fa
Maria Alexeeva <maxaalexeeva@gmail.com>
2022-07-28 09:14:32 -0700

    a small fix: previous version would result in an index out of bounds error

--- src/main/scala/org/clulab/habitus/printer/MentionInfo.scala
+++ src/main/scala/org/clulab/habitus/printer/MentionInfo.scala
@@ -14,7 +14,7 @@ object MentionInfo {
 
   def apply(mention: Mention, inputFilename: String): MentionInfo = {
     val document = mention.document
-    val sentInterval = (math.max(mention.sentence - windowSize, 0), math.min(mention.sentence + windowSize + 1, mention.document.sentences.length + 1))
+    val sentInterval = (math.max(mention.sentence - windowSize, 0), math.min(mention.sentence + windowSize + 1, mention.document.sentences.length))
     val contextWindow = mention.document.sentences.slice(sentInterval._1, sentInterval._2).map(_.getSentenceText).mkString(" ")
     val sentenceText = document.sentences(mention.sentence).getSentenceText
     val label = mention.label
