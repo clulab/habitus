8be218789794db1f3ce7e0d8d302835d496fa711
Keith Alcock <github@keithalcock.com>
2023-04-20 11:38:01 -0700

    Use cosine similarity

--- python/interview_to_beliefs/interview_to_beliefs.py
+++ python/interview_to_beliefs/interview_to_beliefs.py
@@ -4,30 +4,35 @@ import pandas
 from sentence_transformers import SentenceTransformer
 
 if __name__ == "__main__":
-    beliefs_file = "../../../corpora/beliefs_with_metadata_by_conf_score/senegal_hr_queries_beliefs_80conf_with_metadata.tsv" # sys.argv[1]
-    interviews_file = "../../../corpora/experiment_corpus/row_labels_harvest.csv" # sys.argv[2]
+    base_dir_name = "../../../corpora/"
+    # beliefs_path_name = sys.argv[1]
+    beliefs_path_name = base_dir_name + "beliefs_with_metadata_by_conf_score/senegal_hr_queries_beliefs_80conf_with_metadata.tsv"
+    # interview_path_name = sys.argv[2]
+    interview_path_name = base_dir_name + "experiment_corpus/row_labels_harvest.csv"
     encoding = "utf-8"
     k = 5
 
-    beliefs_dataframe = pandas.read_table(beliefs_file, index_col = 0, header = 0, encoding = encoding)
+    beliefs_dataframe = pandas.read_table(beliefs_path_name, index_col = 0, header = 0, encoding = encoding)
     beliefs = beliefs_dataframe["belief"].tolist()
 
-    interviews_dataframe = pandas.read_csv(interviews_file, index_col = 0, header = 0, encoding = encoding)
-    interviews = interviews_dataframe["readable"].tolist()
+    interview_dataframe = pandas.read_csv(interview_path_name, index_col = 0, header = 0, encoding = encoding)
+    interview_sentences = interview_dataframe["readable"].tolist()
 
     model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+    # These embeddings appear to be normalized.
     beliefs_embeddings = model.encode(beliefs)
-    interviews_embeddings = model.encode(interviews)
+    interview_sentences_embeddings = model.encode(interview_sentences)
 
     width = beliefs_embeddings.shape[1]
-    index = faiss.IndexFlatL2(width)
+    # index = faiss.IndexFlatL2(width) # This is for 2-D Euclidean distance.
+    index = faiss.IndexFlatIP(width) # IP is inner product, so cosine similarity given normalized vectors.
     index.add(beliefs_embeddings)
-    print(index.is_trained)
+    assert index.is_trained
 
-    _, beliefs_indexes_list = index.search(interviews_embeddings, k)
+    _, beliefs_indexes_collection = index.search(interview_sentences_embeddings, k)
 
-    for index, beliefs_indexes in enumerate(beliefs_indexes_list):
-        interview = interviews[index]
-        print(interview)
+    for index, beliefs_indexes in enumerate(beliefs_indexes_collection):
+        interview_sentence = interview_sentences[index]
+        print(interview_sentence)
         for beliefs_index in beliefs_indexes:
             print("\t" + beliefs[beliefs_index])
