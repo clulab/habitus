4884d0bc52e38c73ed936dd07377ef4f16fb85e8
Keith Alcock <github@keithalcock.com>
2023-07-12 17:49:36 -0700

    Add file, sentence, and context

--- src/main/scala/org/clulab/habitus/apps/ExtractCausationFromDirectoryApp.scala
+++ src/main/scala/org/clulab/habitus/apps/ExtractCausationFromDirectoryApp.scala
@@ -9,6 +9,7 @@ import scala.util.Using
 
 object ExtractCausationFromDirectoryApp extends App with Logging {
   implicit val formats: DefaultFormats.type = org.json4s.DefaultFormats
+  val contextWindow = 2
 
   val inputDir = args(0)
   val outputFile = args(1)
@@ -16,26 +17,55 @@ object ExtractCausationFromDirectoryApp extends App with Logging {
   val files = FileUtils.findFiles(inputDir, "jsonld")
 
   Using.resource(FileUtils.printWriterFromFile(outputFile)) { printWriter =>
-//    printWriter.println(s"file\tcausation")
-    printWriter.println("causation")
+    printWriter.println(s"file\tcausation\tsentence\tcontext")
     files.foreach { file =>
       try {
-        val text = FileUtils.getTextFromFile(file)
-        val jValue = JsonMethods.parse(text)
-        val jObject = jValue.extract[JObject]
-        val extractions = (jObject \ "extractions").extractOpt[JArray]
+        val json = FileUtils.getTextFromFile(file)
+        val root = JsonMethods.parse(json).extract[JObject]
+        val documents = (root \ "documents").extract[JArray]
+
+        require(documents.arr.length == 1)
+
+        val sentences = (documents(0) \ "sentences").extractOpt[JArray]
+            .map(_.arr)
+            .getOrElse(List.empty[JValue])
+        val sentenceIdAndTextTuples = sentences.map { sentence =>
+          val id = (sentence \ "@id").extract[String]
+          val text = (sentence \ "text").extract[String]
+
+          id -> text
+        }
+
+        val extractions = (root \ "extractions").extractOpt[JArray]
             .map(_.arr)
             .getOrElse(List.empty[JValue])
         val causations = extractions.filter { extraction =>
           (extraction \ "subtype").extract[String] == "causation"
         }
-        val texts = causations.map { causation =>
-          (causation \ "text").extract[String]
+        val textSentenceContextTuples = causations.map { causation =>
+          val text = (causation \ "text").extract[String]
+          val provenances = (causation \ "provenance").extract[JArray]
+
+          require(provenances.arr.length == 1)
+
+          val causationSentenceId = (provenances(0) \ "sentence" \ "@id").extract[String]
+          val sentenceIndex = sentenceIdAndTextTuples.indexWhere { case (sentenceId, _) =>
+            causationSentenceId == sentenceId
+          }
+          val sentence = sentenceIdAndTextTuples(sentenceIndex)._2
+          val contextRange = Range.inclusive(sentenceIndex - contextWindow, sentenceIndex + contextWindow)
+          val contextSentences = contextRange.flatMap { sentenceIndex =>
+            val lifted = sentenceIdAndTextTuples.lift(sentenceIndex).map { sth => sth._2 }
+
+            lifted
+          }
+          val context = contextSentences.mkString(" ")
+
+          (text, sentence, context)
         }
 
-        texts.foreach { text =>
-//          printWriter.println(s"${file.getName}\t$text")
-          printWriter.println(text)
+        textSentenceContextTuples.foreach { case (text, sentence, context) =>
+          printWriter.println(s"${file.getName}\t$text\t$sentence\t$context")
         }
       }
       catch {
