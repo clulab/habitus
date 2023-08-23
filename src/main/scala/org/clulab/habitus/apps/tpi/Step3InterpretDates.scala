5a322f5b78a6e522215d9d8f19b034f7d0063f45
Keith Alcock <github@keithalcock.com>
2023-08-23 10:15:12 -0700

    Continue

--- src/main/scala/org/clulab/habitus/apps/tpi/Step3InterpretDates.scala
+++ src/main/scala/org/clulab/habitus/apps/tpi/Step3InterpretDates.scala
@@ -11,18 +11,22 @@ object Step3InterpretDates extends App with Logging {
   val outputFileName = "../corpora/multi/CausalBeliefsDate.tsv"
 
   val patterns = Seq(
+    "date",
     "\\d\\d\\d\\d-\\d\\d-\\d\\dT\\d\\d:\\d\\d:\\d\\d\\+\\d\\d:\\d\\d", // 2022-03-29T14:05:04+00:00
-    "(July|August) \\d\\d?, \\d\\d\\d\\d", // August 21, 2016
-    "(February|July|November) \\d\\d?, \\d\\d\\d\\d \\d\\d?:\\d\\d [ap]m", // July 5, 2023 3:26 pm
+    "(January|February|March|April|May|June|July|August|September|October|November|December) \\d\\d?, \\d\\d\\d\\d", // August 21, 2016
+    "(January|February|March|April|May|June|July|August|September|October|November|December) \\d\\d?, \\d\\d\\d\\d \\d\\d?:\\d\\d [ap]m", // July 5, 2023 3:26 pm
     "\\d\\d?-\\d\\d?-\\d\\d\\d\\d  \\d\\d?:\\d\\d:\\d\\d [AP]M"  //
   )
 
   def convertDate(date: String): String = {
 
-    if (!patterns.exists(Pattern.matches(_,date)))
+    if (date.nonEmpty && !patterns.exists(Pattern.matches(_,date))) {
       print(s"$date is unmatched!")
+      ""
+    }
+    else
 
-//    if (Pattern.matches(pattern1, date))
+    //    if (Pattern.matches(pattern1, date))
 //      println("pattern1!") // OK
 //    if (Pattern.matches(pattern2, date))
 //      println("pattern2!")
@@ -41,7 +45,11 @@ object Step3InterpretDates extends App with Logging {
 
     inputSource.getLines.foreach { line =>
       val Array(_, _, date) = tsvReader.readln(line, 3)
-        println(s"$date -> ${convertDate(date)}")
+      val convertedDate = convertDate(date)
+
+      if (convertedDate.isEmpty)
+        println(line)
+      println(s"$date -> $convertedDate")
     }
   }
 }
