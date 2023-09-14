bb4832e8b9619ab7233b5d8bf57fc23292af02e0
Keith Alcock <github@keithalcock.com>
2023-09-14 08:36:22 -0700

    Improve step 1

--- src/main/scala/org/clulab/habitus/apps/tpi/Step1OutputEidos.scala
+++ src/main/scala/org/clulab/habitus/apps/tpi/Step1OutputEidos.scala
@@ -1,6 +1,8 @@
 package org.clulab.habitus.apps.tpi
 
 import ai.lum.common.FileUtils._
+import com.typesafe.config.ConfigValueFactory
+import org.clulab.utils.ThreadUtils
 import org.clulab.wm.eidos.EidosSystem
 import org.clulab.wm.eidos.serialization.jsonld.JLDCorpus
 import org.clulab.wm.eidoscommon.utils.{FileEditor, FileUtils}
@@ -8,19 +10,24 @@ import org.json4s.DefaultFormats
 import org.json4s.jackson.JsonMethods
 
 import java.io.File
+import java.util.concurrent.atomic.AtomicInteger
 import scala.util.Using
 
 object Step1OutputEidos extends App {
   implicit val formats: DefaultFormats.type = org.json4s.DefaultFormats
 
-  val baseDirectoryName = "../corpora/multi"
-  val files = new File(baseDirectoryName).listFilesByWildcard("*.json", recursive = true)
-  val eidosSystem = new EidosSystem()
+  val baseDirectoryName = args.lift(0).getOrElse("../corpora/multi3/crop")
+  val files = new File(baseDirectoryName).listFilesByWildcard("*.json", recursive = true).toList
+  val config =  EidosSystem.defaultConfig
+      .withValue("ontologies.useGrounding", ConfigValueFactory.fromAnyRef(false))
+  val eidosSystem = new EidosSystem(config)
+  val count = new AtomicInteger()
+  val parFiles = ThreadUtils.parallelize(files, 8)
 
-  files.zipWithIndex.par.foreach { case (file, index) =>
+  parFiles.foreach { file =>
     try {
       val path = FileEditor(file.getCanonicalFile).setExt("jsonld").get
-      println(s"$index ${file.getCanonicalPath} -> ${path.getCanonicalPath}")
+      println(s"${count.getAndIncrement()} ${file.getCanonicalPath} -> ${path.getCanonicalPath}")
 
       val json = FileUtils.getTextFromFile(file)
       val jValue = JsonMethods.parse(json)
