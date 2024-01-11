a33a29a00683fe08d2adaff6cc60a1585e75855d
Keith Alcock <github@keithalcock.com>
2024-01-11 11:34:06 -0700

    Update sitemap scraper

--- scraper/src/main/scala/org/clulab/habitus/scraper/apps/ScrapeSitemapsApp.scala
+++ scraper/src/main/scala/org/clulab/habitus/scraper/apps/ScrapeSitemapsApp.scala
@@ -1,48 +1,21 @@
 package org.clulab.habitus.scraper.apps
 
+import org.clulab.utils.{FileUtils, StringUtils}
 import sttp.client4.quick._
 import sttp.model.{MediaType, Uri}
 
+import java.io.PrintWriter
 import scala.io.Source
+import scala.util.Using
 import scala.xml.{Elem, XML, Source => XMLSource}
 
 object ScrapeSitemapsApp extends App {
   val urlString = args.lift(0).getOrElse("https://3news.com")
+  val outFileName = args.lift(1).getOrElse(StringUtils.afterLast(urlString, '/') + ".pages")
+
   val robotsString = urlString + "/robots.txt"
   val sitemapIndexString = urlString + "/sitemap_index.xml"
   val sitemapString = urlString + "/sitemap.xml"
-  val sitemapIndexes = {
-    val sitemapIndexes = getSitemapIndexesFromRobots(robotsString)
-    sitemapIndexes.foreach(println)
-    val distinctSitemapIndexes = (sitemapIndexes :+ sitemapIndexString).distinct
-
-    distinctSitemapIndexes
-  }
-  val sitemaps = {
-    val sitemaps = sitemapIndexes.flatMap { sitemapIndex =>
-      val sitemaps = getSitemapsFromSitemapIndex(sitemapIndex)
-      sitemaps.foreach(println)
-      sitemaps
-    }
-    val distinctSitemaps = (sitemaps :+ sitemapString).distinct
-
-    distinctSitemaps
-  }
-  val sites = {
-    val sites = sitemaps.flatMap { sitemap =>
-      val sites = getSitesFromSitemap(sitemap)
-      sites.foreach(println)
-      sites
-    }
-    val distinctSites = sites.distinct
-
-    distinctSites
-  }
-
-  println()
-  println("=" * 50)
-  println()
-  sites.foreach(println)
 
   def getSitemapIndexesFromRobots(robotsString: String): Seq[String] = {
     val header = "Sitemap:"
@@ -126,5 +99,52 @@ object ScrapeSitemapsApp extends App {
       else
         getSitesFromElem(xml)
     }
+  }
+
+  def run(printWriter: PrintWriter): Unit = {
+
+    def println(string: String): Unit = {
+      printWriter.println(string)
+      System.out.println(string)
+    }
+
+    println("Sitemap indexes:\n")
+    val sitemapIndexes = {
+      val sitemapIndexes = getSitemapIndexesFromRobots(robotsString)
+      sitemapIndexes.foreach(println)
+      val distinctSitemapIndexes = (sitemapIndexes :+ sitemapIndexString).distinct
+
+      distinctSitemapIndexes
+    }
+    println("\n")
+    println("Sitemaps:\n")
+    val sitemaps = {
+      val sitemaps = sitemapIndexes.flatMap { sitemapIndex =>
+        val sitemaps = getSitemapsFromSitemapIndex(sitemapIndex)
+        sitemaps.foreach(println)
+        sitemaps
+      }
+      val distinctSitemaps = (sitemaps :+ sitemapString).distinct
+
+      distinctSitemaps
+    }
+    println("\n")
+    println("Sites:\n")
+    val sites = {
+      val sites = sitemaps.flatMap { sitemap =>
+        val sites = getSitesFromSitemap(sitemap)
+        sites.foreach(println)
+        sites
+      }
+      val distinctSites = sites.distinct
+
+      distinctSites
+    }
+    println("\n")
+    println("Finished!")
+  }
+
+  Using.resource(FileUtils.printWriterFromFile(outFileName)) { printWriter =>
+    run(printWriter)
   }
 }
