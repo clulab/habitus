aa2062228379dce3be7a5107bd1973d79f920886
Keith Alcock <github@keithalcock.com>
2024-01-17 12:25:56 -0700

    Put tpi sentiment stage back in
    Scala files will need to be adjusted to account for the additional column!

--- belief_pipeline/tpi_main.py
+++ belief_pipeline/tpi_main.py
@@ -5,6 +5,7 @@ from tpi_belief_stage import TpiBeliefStage
 from tpi_input_stage import TpiInputStage
 from tpi_location_stage import TpiLocationStage
 from tpi_resolution_stage import TpiResolutionStage
+from tpi_sentiment_stage import TpiSentimentStage
 from typing import Tuple
 
 
@@ -17,15 +18,17 @@ def get_in_and_out() -> Tuple[str, str]:
 
 if __name__ == "__main__":
     belief_model_name: str = "maxaalexeeva/belief-classifier_mturk_unmarked-trigger_bert-base-cased_2023-4-26-0-34"
+    sentiment_model_name: str = "hriaz/finetuned_beliefs_sentiment_classifier_experiment1"
     locations_file_name: str = "./belief_pipeline/UG.tsv"    
-    input_file_name: str = "../corpora/uganda/uganda.tsv"
-    output_file_name: str = "../corpora/uganda/uganda2.tsv"
+    input_file_name: str = "../corpora/uganda-tsv-sentiment/uganda1000.tsv"
+    output_file_name: str = "../corpora/uganda-tsv-sentiment/uganda1000-2.tsv"
     # input_file_name, output_file_name = get_in_and_out()
     pipeline = Pipeline(
         TpiInputStage(input_file_name),
         [
             TpiResolutionStage(),
             TpiBeliefStage(belief_model_name),
+            TpiSentimentStage(sentiment_model_name),
             TpiLocationStage(locations_file_name)
         ],
         PandasOutputStage(output_file_name)
---
+++ belief_pipeline/tpi_sentiment_stage.py
@@ -0,0 +1,31 @@
+from pandas import DataFrame
+from pipeline import InnerStage
+from transformers import pipeline
+
+import math
+
+class TpiSentimentStage(InnerStage):
+    def __init__(self, model_name: str, belief_column_name: str = "belief", text_column_name: str = "sentence") -> None:
+        super().__init__()
+        self.sentiment_analysis = pipeline("sentiment-analysis", model=model_name, device="cpu")
+        self.belief_column_name = belief_column_name
+        self.text_column_name = text_column_name
+
+    def run(self, data_frame: DataFrame) -> DataFrame:
+        sentiment_scores = []
+        for _, row in data_frame.iterrows():
+            belief = row[self.belief_column_name]
+            if belief:
+                text = row[self.text_column_name]
+                sentiment_dict = self.sentiment_analysis(text)
+                label, score = sentiment_dict[0]["label"], sentiment_dict[0]["score"]
+                if label == "NEGATIVE":
+                    score *= -1
+                elif label == "UNDETERMINED":
+                    score = 0
+            else:
+                score = math.nan
+            sentiment_scores.append(score)
+        data_frame["sentiment_scores"] = sentiment_scores
+        return data_frame
+    
