13d118fdc505cb46766511bc03262e7cf01ff75a
Keith Alcock <github@keithalcock.com>
2024-02-01 16:00:18 -0700

    Add DatasetRecord

--- elasticsearch/src/main/scala/org/clulab/habitus/elasticsearch/apps/DatasetToElasticsearchApp.scala
+++ elasticsearch/src/main/scala/org/clulab/habitus/elasticsearch/apps/DatasetToElasticsearchApp.scala
@@ -19,6 +19,7 @@ import org.elasticsearch.client.{Request, RestClient}
 import org.elasticsearch.client.RestClientBuilder.HttpClientConfigCallback
 
 import java.io.{File, FileInputStream}
+import java.lang.{Boolean => JBoolean, Integer => JInteger, Float => JFloat}
 import java.util.HashMap
 import java.util.Properties
 import scala.util.Using
@@ -32,14 +33,25 @@ case class CauseOrEffect(
 ) {
 
   def serialize(): HashMap[String, AnyRef] = {
-    ???
+    val map = new HashMap[String, AnyRef]()
+
+    map.put("text", text)
+    map.put("incCount", incCount.asInstanceOf[JInteger])
+    map.put("decCount", decCount.asInstanceOf[JInteger])
+    map.put("posCount", posCount.asInstanceOf[JInteger])
+    map.put("negCount", negCount.asInstanceOf[JInteger])
+    map
   }
 }
 
 case class Relation(cause: CauseOrEffect, effect: CauseOrEffect) {
 
   def serialize(): HashMap[String, AnyRef] = {
-    ???
+    val map = new HashMap[String, AnyRef]()
+
+    map.put("cause", cause.serialize())
+    map.put("effect", effect.serialize())
+    map
   }
 }
 
@@ -50,16 +62,17 @@ case class CausalRelation(
 ) {
 
   def serialize(): HashMap[String, AnyRef] = {
-    ???
+    val map = new HashMap[String, AnyRef]()
+
+    map.put("index", index.asInstanceOf[JInteger])
+    map.put("negationCount", index.asInstanceOf[JInteger])
+    if (relations.nonEmpty)
+      map.put("relations", relations.map(_.serialize))
+    map
   }
 }
 
-case class LatLon(lat: Float, lon: Float) {
-
-  def serialize(): HashMap[String, AnyRef] = {
-    ???
-  }
-}
+case class LatLon(lat: Float, lon: Float)
 
 case class Location(
   val name: String,
@@ -67,7 +80,11 @@ case class Location(
 ) {
 
   def serialize(): HashMap[String, AnyRef] = {
-    ???
+    val map = new HashMap[String, AnyRef]()
+
+    map.put("name", name)
+    map.put("location", Array(latLon.lat, latLon.lon))
+    map
   }
 }
 
@@ -96,6 +113,7 @@ case class DatasetRecord(
   nextDistance: Int
 ) {
 
+  // See advice at https://discuss.elastic.co/t/ways-to-build-json-doc-in-es8-java-api-client/314459.
   def serialize(): HashMap[String, AnyRef] = {
     val map = new HashMap[String, AnyRef]()
 
@@ -103,6 +121,29 @@ case class DatasetRecord(
     map.put("region", region)
     map.put("url", url)
     map.put("title", title)
+    if (terms.nonEmpty)
+      map.put("terms", terms)
+    map.put("dateline", dateline)
+    map.put("date", date)
+    map.put("byline", byline)
+    map.put("sentenceIndex", sentenceIndex.asInstanceOf[JInteger])
+    map.put("sentence", sentence)
+    if (causalRelations.nonEmpty)
+      map.put("causalRelations", causalRelations.map(_.serialize))
+    map.put("isBelief", isBelief.asInstanceOf[JBoolean])
+    map.put("sentiment", sentiment)
+    if (sentenceLocations.nonEmpty)
+      map.put("sentenceLocations", sentenceLocations.map(_.serialize))
+    map.put("contextBefore", contextBefore)
+    map.put("contextAfter", contextAfter)
+    if (contextLocations.nonEmpty)
+      map.put("contextLocations", contextLocations.map(_.serialize))
+    if (prevLocations.nonEmpty)
+      map.put("prevLocations", prevLocations.map(_.serialize))
+    map.put("prevDistance", prevDistance.asInstanceOf[JInteger])
+    if (nextLocations.nonEmpty)
+      map.put("nextLocations", nextLocations.map(_.serialize))
+    map.put("nextDistance", nextDistance.asInstanceOf[JInteger])
     map
   }
 }
