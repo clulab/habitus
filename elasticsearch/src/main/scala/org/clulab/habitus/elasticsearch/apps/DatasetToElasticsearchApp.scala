5b488a34eb35734b2e1023b53dac3043dfc8e2d1
Keith Alcock <github@keithalcock.com>
2024-02-01 10:16:55 -0700

    Add some data

--- elasticsearch/src/main/scala/org/clulab/habitus/elasticsearch/apps/DatasetToElasticsearchApp.scala
+++ elasticsearch/src/main/scala/org/clulab/habitus/elasticsearch/apps/DatasetToElasticsearchApp.scala
@@ -1,8 +1,9 @@
 package org.clulab.habitus.elasticsearch.apps
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient
+import co.elastic.clients.elasticsearch._types.query_dsl.{IdsQuery, Query}
 import co.elastic.clients.elasticsearch.cat.{IndicesRequest, IndicesResponse}
-import co.elastic.clients.elasticsearch.core.{IndexRequest, IndexResponse}
+import co.elastic.clients.elasticsearch.core.{IndexRequest, IndexResponse, SearchRequest, SearchResponse}
 import co.elastic.clients.json.jackson.JacksonJsonpMapper
 import co.elastic.clients.elasticsearch.indices.{CreateIndexRequest, CreateIndexResponse, ElasticsearchIndicesClient}
 import co.elastic.clients.transport.rest_client.RestClientTransport
@@ -156,20 +157,171 @@ object DatasetToElasticsearchApp extends App {
     indicesResponse
   }
 
+  def mkDatasetRow(): DatasetRow = {
+    val datasetRow = DatasetRow(
+      dataset = "uganda.tsv",
+      region = "uganda",
+      url = "http://clulab.org",
+      titleOpt = Some("This article is about that"),
+      terms = Seq("mining", "galamsey"),
+      dateline = "September 2, 2023",
+      date = "2023-09-02",
+      byline = "Your local reporter",
+      sentenceIndex = 0,
+      sentence = "The quick brown fox jumped over the lazy dog.",
+      causalRelations = Seq(
+        CausalRelation(
+          index = 1,
+          negationCount = 1,
+          relations = Seq(
+            Relation(
+              cause = CauseOrEffect(
+                text = "cause1-1",
+                incCount = 1,
+                decCount = 2,
+                posCount = 3,
+                negCount = 4
+              ),
+              effect = CauseOrEffect(
+                text = "effect1-1",
+                incCount = 2,
+                decCount = 3,
+                posCount = 4,
+                negCount = 5
+              )
+            ),
+            Relation(
+              cause = CauseOrEffect(
+                text = "cause1-2",
+                incCount = 1,
+                decCount = 2,
+                posCount = 3,
+                negCount = 4
+              ),
+              effect = CauseOrEffect(
+                text = "effect1-2",
+                incCount = 1,
+                decCount = 2,
+                posCount = 3,
+                negCount = 4
+              )
+            )
+          )
+        ),
+        CausalRelation(
+          index = 2,
+          negationCount = 1,
+          relations = Seq(
+            Relation(
+              cause = CauseOrEffect(
+                text = "cause2-1",
+                incCount = 1,
+                decCount = 2,
+                posCount = 3,
+                negCount = 4
+              ),
+              effect = CauseOrEffect(
+                text = "effect2-1",
+                incCount = 2,
+                decCount = 3,
+                posCount = 4,
+                negCount = 5
+              )
+            ),
+            Relation(
+              cause = CauseOrEffect(
+                text = "cause2-2",
+                incCount = 1,
+                decCount = 2,
+                posCount = 3,
+                negCount = 4
+              ),
+              effect = CauseOrEffect(
+                text = "effect2-2",
+                incCount = 1,
+                decCount = 2,
+                posCount = 3,
+                negCount = 4
+              )
+            )
+          )
+        )
+      ),
+      isBelief = true,
+      sentiment = "POSITIVE",
+      sentenceLocations = Seq(
+        Location(
+          name = "sentenceLocation1",
+          latLon = LatLon(1f, 2f)
+        ),
+        Location(
+          name = "sentenceLocation2",
+          latLon = LatLon(3f, 4f)
+        )
+      ),
+      contextBefore = "This is what came before",
+      contextAfter = "This is what came after",
+      contextLocations = Seq(
+        Location(
+          name = "contextLocation1",
+          latLon = LatLon(5f, 6f)
+        ),
+        Location(
+          name = "contextLocation2",
+          latLon = LatLon(7f, 8f)
+        )
+      ),
+      prevLocations = Seq(
+        Location(
+          name = "prevLocation1",
+          latLon = LatLon(9f, 10f)
+        ),
+        Location(
+          name = "prevLocation2",
+          latLon = LatLon(11f, 12f)
+        )
+      ),
+      prevDistance = 4,
+      nextLocations = Seq(
+        Location(
+          name = "nextLocation1",
+          latLon = LatLon(13f, 14f)
+        ),
+        Location(
+          name = "nextLocation2",
+          latLon = LatLon(15f, 16f)
+        )
+      ),
+      nextDistance = 5
+    )
+
+    datasetRow
+  }
+
+  def runSearch(elasticsearchClient: ElasticsearchClient, indexName: String, docId: String): String = {
+    val idsQuery: IdsQuery = new IdsQuery.Builder().values(docId).build()
+    val query: Query = new Query.Builder().ids(idsQuery).build() // What does this have to look like?
+    val searchRequest = new SearchRequest.Builder().index(indexName).query(query).build()
+    val searchResponse = elasticsearchClient.search(searchRequest, classOf[String])
+    val searchResponseString = searchResponse.toString
+
+    println(searchResponseString)
+    searchResponseString
+  }
+
   def run(): Unit = {
     val elasticsearchClient = mkElasticsearchClient(credentialsFilename)
-//    val datasetRow = DatasetRow(
-//
-//    )
 
     // runCreateIndex(elasticsearchClient, indexName)
-    runIndices(elasticsearchClient)
-    //runIndex(elasticsearchClient, indexName, datasetRow)
+    // runIndices(elasticsearchClient)
+    // runIndex(elasticsearchClient, indexName, mkDatasetRow())
+    runSearch(elasticsearchClient, indexName, "pydJYo0BaDron0AvRCXx")
 
     println("Goodbye")
   }
 
   try {
+    // How can the process be make to stop?
     run()
   }
   catch {
