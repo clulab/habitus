09b269fc8d9a3a9c288ead5f529851af1167e0dd
Keith Alcock <github@keithalcock.com>
2024-03-11 19:49:12 -0700

    Clean

--- src/main/scala/org/clulab/habitus/apps/grid/Csv2Tsv.scala
+++ src/main/scala/org/clulab/habitus/apps/grid/Csv2Tsv.scala
@@ -4,13 +4,9 @@ import org.clulab.utils.{FileUtils, Sourcer}
 
 import scala.util.Using
 
-
-// Change all commas outside of quoted strings to tabs.
-// Remove double quotes.
-// Replace some special characters like tab itself.
 object Csv2Tsv extends App {
-  val csvFilename = args.lift(0).getOrElse("../corpora/grid/uq500-karamoja/in/uq500-karamoja.csv")
-  val tsvFilename = args.lift(1).getOrElse("../corpora/grid/uq500-karamoja/in/uq500-karamoja.tsv")
+  val csvFilename = args.lift(0).getOrElse("../corpora/grid/uq500-karamoja/csvcheck.csv")
+  val tsvFilename = args.lift(1).getOrElse("../corpora/grid/uq500-karamoja/csvcheck.tsv")
 
   trait State
   object OutsideFieldState extends State
@@ -23,18 +19,24 @@ object Csv2Tsv extends App {
 
     Using.resource(FileUtils.printWriterFromFile(tsvFilename)) { printWriter =>
 
-      def outputString(string: String): Unit = printWriter.print(string)
+      def printString(string: String): Unit = printWriter.print(string)
 
-      def outputChar(char: Char): Unit = printWriter.print(char)
+      def printChar(char: Char): Unit = printWriter.print(char)
 
-      def outputNewline(): Unit = printWriter.println
+      def printLine(): Unit = printWriter.println
 
       def throwChar(char: Char, state: State): Nothing = {
         throw new RuntimeException(s"Char '$char' is invalid in state $state.")
       }
 
+      def throwString(string: String): Nothing = {
+        throw new RuntimeException(string)
+      }
+
       val nextState = lines.foldLeft(OutsideFieldState: State) { (state, line) =>
+        println(line)
         val nextState = line.foldLeft(state: State) { (state, char) =>
+          println(char)
           val nextState = state match {
             case OutsideFieldState =>
               // I should only see a quote or some char that starts the field.
@@ -45,31 +47,31 @@ object Csv2Tsv extends App {
                 case ','  => throwChar(char, state)
                 case '"'  => InsideQuotedFieldState
                 case '\\' => throwChar(char, state)
-                case _    => outputChar(char); InsideFieldState
+                case _    => printChar(char); InsideFieldState
               }
               nextState
             case InsideFieldState =>
               // I need to escape special characters and watch for the looming comma.
               val nextState = char match {
-                case '\n' => outputString("\\n"); InsideFieldState
-                case '\r' => outputString("\\r"); InsideFieldState
-                case '\t' => outputString("\\t"); InsideFieldState
-                case ','  => outputChar('\t'); OutsideFieldState
+                case '\n' => printString("\\n"); InsideFieldState
+                case '\r' => printString("\\r"); InsideFieldState
+                case '\t' => printString("\\t"); InsideFieldState
+                case ','  => printChar('\t'); OutsideFieldState
                 case '"'  => throwChar(char, state)
-                case '\\' => outputString("\\\\"); InsideFieldState
-                case _    => outputChar(char); InsideFieldState
+                case '\\' => printString("\\\\"); InsideFieldState
+                case _    => printChar(char); InsideFieldState
               }
               nextState
             case InsideQuotedFieldState =>
               // I need to escape special characters and watch for the looming end quote or doubled false alarms.
               val nextState = char match {
-                case '\n' => outputString("\\n"); InsideQuotedFieldState
-                case '\r' => outputString("\\r"); InsideQuotedFieldState
-                case '\t' => outputString("\\t"); InsideQuotedFieldState
-                case ','  => outputChar(char); InsideQuotedFieldState
+                case '\n' => printString("\\n"); InsideQuotedFieldState
+                case '\r' => printString("\\r"); InsideQuotedFieldState
+                case '\t' => printString("\\t"); InsideQuotedFieldState
+                case ','  => printChar(char); InsideQuotedFieldState
                 case '"'  => InsideQuotedQuoteState
-                case '\\' => outputString("\\\\"); InsideQuotedFieldState
-                case _    => outputChar(char); InsideQuotedFieldState
+                case '\\' => printString("\\\\"); InsideQuotedFieldState
+                case _    => printChar(char); InsideQuotedFieldState
               }
               nextState
             case InsideQuotedQuoteState =>
@@ -78,8 +80,8 @@ object Csv2Tsv extends App {
                 case '\n' => throwChar(char, state)
                 case '\r' => throwChar(char, state)
                 case '\t' => throwChar(char, state)
-                case ','  => outputChar('\t'); OutsideFieldState // We are now outside the field.
-                case '"'  => outputChar('"'); InsideQuotedFieldState // It was a double quote and we're still inside the field.
+                case ','  => printChar('\t'); OutsideFieldState // We are now outside the field.
+                case '"'  => printChar('"'); InsideQuotedFieldState // It was a double quote and we're still inside the field.
                 case '\\' => throwChar(char, state)
                 case _    => throwChar(char, state)
               }
@@ -89,10 +91,10 @@ object Csv2Tsv extends App {
         }
         // The line is finished.
         nextState match {
-          case OutsideFieldState => outputNewline(); OutsideFieldState
-          case InsideFieldState => outputNewline(); OutsideFieldState
-          case InsideQuotedFieldState => outputString("\\n"); InsideQuotedFieldState
-          case InsideQuotedQuoteState => outputNewline(); OutsideFieldState
+          case OutsideFieldState => printLine(); OutsideFieldState
+          case InsideFieldState => printLine(); OutsideFieldState
+          case InsideQuotedFieldState => printString("\\n"); InsideQuotedFieldState
+          case InsideQuotedQuoteState => printLine(); OutsideFieldState
         }
         nextState
       }
@@ -100,7 +102,7 @@ object Csv2Tsv extends App {
       nextState match {
         case OutsideFieldState => assert(true)
         case InsideFieldState => assert(false)
-        case InsideQuotedFieldState => throw new RuntimeException("Quoted field was not terminated correctly.")
+        case InsideQuotedFieldState => throwString("Quoted field was not terminated correctly.")
         case InsideQuotedQuoteState => assert(false)
       }
     }
