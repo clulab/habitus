45bd0f280b6b1c0c18d36d8ae23ba26592fd82fb
Keith Alcock <github@keithalcock.com>
2024-04-05 18:01:26 -0700

    Run experiment files

--- belief_pipeline/tpi_main.py
+++ belief_pipeline/tpi_main.py
@@ -19,9 +19,9 @@ def get_in_and_out() -> Tuple[str, str]:
 if __name__ == "__main__":
     belief_model_name: str = "maxaalexeeva/belief-classifier_mturk_unmarked-trigger_bert-base-cased_2023-4-26-0-34"
     sentiment_model_name: str = "hriaz/finetuned_beliefs_sentiment_classifier_experiment1"
-    locations_file_name: str = "./belief_pipeline/UG.tsv"    
-    input_file_name: str = "../corpora/uganda/interview/interviews.tsv"
-    output_file_name: str = "../corpora/uganda/interview/interviews-a.tsv"
+    locations_file_name: str = "./belief_pipeline/SN.tsv"    
+    input_file_name: str = "../corpora/senegal/experiment/experiment.tsv"
+    output_file_name: str = "../corpora/senegal/experiment/experiment-a.tsv"
     # input_file_name, output_file_name = get_in_and_out()
     pipeline = Pipeline(
         TpiInputStage(input_file_name),
