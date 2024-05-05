415652090d22c80725cbf722b82d6ded38816f45
Keith Alcock <github@keithalcock.com>
2024-05-04 21:02:35 -0700

    Filter the logs

---
+++ scraper/src/main/scala/org/clulab/habitus/scraper/apps/FilterLogApp.scala
@@ -0,0 +1,29 @@
+package org.clulab.habitus.scraper.apps
+
+import org.clulab.utils.{FileUtils, Sourcer, StringUtils}
+
+import scala.util.Using
+
+object FilterLogApp extends App {
+  val inFileName = args.lift(0).getOrElse("/home/kwa/Projects/clulab/habitus-project/habitus/ArticleScraperApp-citifm.log")
+  val outFileName = args.lift(1).getOrElse("/home/kwa/Projects/clulab/habitus-project/habitus/articlecorpus.txt")
+
+  Using.resources(
+    FileUtils.printWriterFromFile(outFileName),
+    Sourcer.sourceFromFilename(inFileName)
+  ) { (printWriter, source) =>
+    val lines = source.getLines
+    val filteredLines = lines.filter { line =>
+      line.endsWith(" failed!")
+    }
+
+    filteredLines.foreach { line =>
+      val newLine = line
+          .drop("Scrape of ".length)
+          .dropRight(" failed!".length)
+
+      printWriter.println(newLine)
+      println(newLine)
+    }
+  }
+}
