aa99316a29d1e043c0b05d59dc7fed962d5ee189
Keith Alcock <github@keithalcock.com>
2024-08-15 16:52:17 -0700

    Scrape Uganda

--- scraper/src/main/scala/org/clulab/habitus/scraper/apps/ArticleScraperApp.scala
+++ scraper/src/main/scala/org/clulab/habitus/scraper/apps/ArticleScraperApp.scala
@@ -7,8 +7,8 @@ import org.clulab.habitus.scraper.scrapers.article.CorpusArticleScraper
 
 object ArticleScraperApp extends App {
   val term = "interview"
-  val corpusFileName = args.lift(0).getOrElse(s"./scraper/corpora/interviews/ghana/articlecorpus.txt")
-  val baseDirName = args.lift(1).getOrElse("../corpora/interviews/ghana/articles")
+  val corpusFileName = args.lift(0).getOrElse(s"./scraper/corpora/interviews/uganda/articlecorpus.txt")
+  val baseDirName = args.lift(1).getOrElse("../corpora/interviews/uganda/articles")
   val corpus = PageCorpus(corpusFileName)
   val scraper = new CorpusArticleScraper(corpus)
   val browser: Browser = new HabitusBrowser()
